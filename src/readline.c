47411ef45f2ecafa6b753750b8a701f8c13cc2d1
cdurrant <cdurrant@well.ox.ac.uk>
2010-09-08 12:59:35 +0100

    modified C source file readline.c

--- src/readline.c
+++ src/readline.c
@@ -1,24 +1,24 @@
 /*  Last edited: Oct 18 14:40 1995 (rmott) */
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<ctype.h>
-#include"cl.h"
+#include"readline.h"
 
 /* functions for reading in lines (C) Richard Mott,, ICRF */
 
 
-void
-uncomment( char *string ) /* truncates string at the first ! */
+void uncomment( char *string ) /* truncates string at the first ! */
 {
   while ( *string != '!' && *string != '#' && *string != 0 ) string++;
   *string = 0;
 }
 
-int
-read_line( FILE *file, char *string ) {
+int read_line( FILE *file, char *string ) {
   int	c;
   int	i=0;
 	
-  if ( file )  {
+  if ( file != NULL )  {
     while(c=getc(file))  {
       if (!i && c==EOF)  return EOF;
       if (i && c==EOF ) return i;
@@ -30,10 +30,9 @@ read_line( FILE *file, char *string ) {
   return EOF;
 }
 
-int
-next_line( FILE *file ) {
+int next_line( FILE *file ) {
   int c;
-  if ( file ) {
+  if ( file != NULL ) {
     while(c=getc(file)) {
       if (feof(file)) return 0;
       if (c=='\n') return 1;
@@ -42,8 +41,7 @@ next_line( FILE *file ) {
   return EOF;
 }
 
-int 
-not_blank( char *string ) /* checks whether string is full of white space */
+int not_blank( char *string ) /* checks whether string is full of white space */
 {
   while ( *string != 0 ) {
     if ( ! isspace(*string) ) return 1;
@@ -56,8 +54,7 @@ not_blank( char *string ) /* checks whether string is full of white space */
 /* reads in successive lines, truncating
    comments and skipping blank lines */
 
-int 
-skip_comments( FILE *file, char *string ) { 
+int skip_comments( FILE *file, char *string ) { 
   int n = EOF;
   
   *string = 0;
@@ -70,3 +67,23 @@ skip_comments( FILE *file, char *string ) {
   }
   return n;
 }
+
+int 
+legal_string( char *string, char **strings, int size, int *value )
+
+/* checks if string is a member os strings, and sets value to the index in the array strings
+returns 1 on success and 0 on failure */
+{
+  int i;
+
+  if ( string )
+    for(i=0;i<size;i++)
+      if ( ! strcmp( string, strings[i] ) )
+	{
+	  *value = i;
+	  return 1;
+	}
+
+  return 0;
+}
+
