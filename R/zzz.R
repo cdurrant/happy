d4903b2334cf1e64eecf1b13cae2d3d1a0036e94
cdurrant <cdurrant@well.ox.ac.uk>
2010-09-08 12:49:21 +0100

    modified zzz.R file

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
-.packageName <- "happy_2.0.1"
+.packageName <- "happy.hbrem_2.0.1"
 
 .First.lib <- function(lib, pkg) {
-  library.dynam('happy', pkg, lib)
+  library.dynam('happy.hbrem', pkg, lib)
 }
