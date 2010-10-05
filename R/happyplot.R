dca97bbb8cbe6669bf2c173bf2b374c0e02f3535
Steffen Möller <steffen_moeller@gmx.de>
2010-10-05 16:07:32 +0200

    Smoothening of code
    Removed plenty of "par" by passing the par as an attribute to the
    invoked function. This helps the need for later resets of the thus
    changed attributes.
    
    Now doing the initialisation of the window prior to showing the text.

--- R/happyplot.R
+++ R/happyplot.R
@@ -1,10 +1,20 @@
                                         # create plots of the result returned by hfit, mfit
 
 happyplot <- function ( fit, mode='logP', labels=NULL, xlab='cM', ylab=NULL, main=NULL, sub=NULL, type='s',
-			vlines.lty=3, # to link labels with graph use dotted lines
+			vlines.lty=3,		# to link labels with graph use dotted lines
 			vlines.col="lightgray", # and those lines should not distract from the main graph too much
+			vlines.lwd=1,		# which is why they are rather thin by default
+			labels.col="black",	# colour for labels (from top downwards)
+			labels.srt=270,		# degree of rotation for labels
+			labels.adj=0,		# adjustment for lables (left-justified)
+			labels.ps=8,		# point size of label text
+			lines.lwd=2,		# width of lines of main plot
+			lines.col="black",	# colour in which draw the main plot
 			pch=20,
 			 ... ) {
+
+  plot.new(...)					# initialisation of graphics, calling it
+						# early to allow use of strwidth
 
   def.par <- par(no.readonly=TRUE)
 
@@ -33,7 +43,7 @@ happyplot <- function ( fit, mode='logP', labels=NULL, xlab='cM', ylab=NULL, mai
     offset <- offset + plots 
   }
 
-                                        # the y-axis range
+						# the y-axis range
   
   mx <- 0
   
@@ -77,47 +87,37 @@ happyplot <- function ( fit, mode='logP', labels=NULL, xlab='cM', ylab=NULL, mai
   rx = range( as.numeric( lp[,1] ) )
   lx <- rx[2]-rx[1]
   tx <- c( rx[1] + 0.02*(lx) )
+  plot.window(xlim=rx,ylim=mx, ...)
 
   ty <- c( 0.95*mx[2] ) 
   text( tx, ty, cnames[offset], adj=c(0))
   wd <- strwidth(cnames)
   buff <- strwidth("spa")
 
-  plot.new(...)
-  plot.window(xlim=rx,ylim=mx, ...)
   title(main=main,sub=sub,xlab=xlab,ylab=ylab, ...)
   axis(side=1)
   axis(side=2)
-						# the main data
-  par(col="black")
-  par(lwd=2)
-  lines( x=lp[,1], y=lp[,offset], type=t, pch=pch, ...)
 
-  if ( rangemax > offset ) 
+						# the main data, not using 'plot' to avoid
+						# empty pages e.g. when printing to PDF
+  lines( x=lp[,1], y=lp[,offset], type=type, pch=pch, col=lines.col, lwd=lines.lwd,...)
+
+  if ( rangemax > offset ) {
     for( i in (offset+1):rangemax ) {
-      par(col=colours[i-offset+1])
+      col=colours[i-offset+1]
       tx <- c( tx[1] + wd[i-1] + buff[1]) 
-      text( tx, ty, cnames[i], adj=c(0))
-      par(ps=1)
-      lines( x=lp[,1], y=lp[,i], t=t, pch=pch)
-      par(ps=12)
+      text( tx, ty, cnames[i], adj=c(0),ps=12,col=col)
+      lines( x=lp[,1], y=lp[,i], type=type, pch=pch,ps=1,col=col)
     }
+  }
 
-  par(col="black")
 						# the labels
   if ( ! is.null(labels) ) {
-    par(srt=270)
-    par(adj=0)
-    par(ps=8)
     y <- rep( mx[2]*0.99, length(labels$text) )
-    text( labels$POSITION, y, as.character(labels$text) )
-    par(lwd=1)
-    par(col='black')
-    x <- labels$POSITION
-    for( m in x) {
-      lines( x=c( m,m ), y=c(0,ymax) , lty=vlines.lty, col=vlines.col)
+    text( labels$POSITION, y, as.character(labels$text), col=labels.col, srt=labels.srt, ps=labels.ps, adj=0 )
+    for( m in labels$POSITION) {
+      lines( x=c( m,m ), y=c(0,ymax) , lty=vlines.lty, col=vlines.col, lwd=vlines.lwd)
     }
-    par(srt=0)
   }
 
   par(def.par)
